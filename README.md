a84be55eb8a1ec7acb77501155d69f8a50908f86
Nathan Rich <nathan.rich@divvypay.com>
2019-11-25 13:43:06 -0700

    Add user_info script

---
+++ README.md
@@ -0,0 +1,5 @@
+# plex-utils
+## user_info
+- Environment variables: 
+`PLEX_TOKENS` comma separated string (no spaces) of plex user tokens
+- Run `get_users.py` to get a list of users' email addresses
---
+++ user_info/get_users.py
@@ -0,0 +1,37 @@
+import requests
+import xmltodict
+import os
+from pprint import pprint
+from datetime import datetime
+
+
+def get_users(plex_token):
+    url = 'https://plex.tv/api/users'
+    params = {'X-Plex-Token': plex_token}
+    response = requests.get(url, params=params)
+    parsed_users = xmltodict.parse(response.text)['MediaContainer']['User']
+    return parsed_users
+
+
+def handler():
+    # comma separated string of tokens
+    plex_tokens = os.environ['PLEX_TOKENS'].split(',')
+
+    all_emails = []
+    user_count = 0
+    for token in plex_tokens:
+        users = get_users(token)
+        server_name = users[0]['Server']['@name']
+        for user in users:
+            # print(f"{user['@title']} -- {user['@email']}")
+            all_emails.append(user['@email'])
+
+        print(f'{server_name} users: {len(users)}')
+        user_count += len(users)
+
+    print(f'Total Users: {user_count}\n')
+    print(','.join(all_emails))
+
+
+if __name__ == '__main__':
+    handler()
